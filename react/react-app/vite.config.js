e2fbffd91090dc0b5ff984106e47abba342f8fb5
Bhawya Misri <bmisri@tran.ad.com>
2025-07-25 18:47:47 +0530

    second commmit

--- react/react-app/vite.config.js
+++ react/react-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base :"flix-yml/react/react-app/"
+  base :"/flix-yml/react/react-app/"
 })
